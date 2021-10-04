d93e0c7d8414d87cfe4e3c120597eb56c28b0caa
Pooja Nambiar <poojanambiar@Poojas-MacBook-Pro.local>
2021-10-04 10:28:28 +0200

    unit tests

---
+++ BlackjackTests/DeckTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using BlackjackApp;
+using NUnit.Framework;
+using System.Linq;
+
+namespace Tests
+{
+    [TestFixture]
+    public class DeckTests
+    {
+        private Deck _deck;
+        private Deck _shuffledDeck;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _deck = new Deck();
+            _deck.CardDeck = _deck.BuildCardDeck();
+            _shuffledDeck = new Deck();
+        }
+
+        [Test]
+        public void BuildCardDeckSuccessTest()
+        {
+            Assert.IsNotNull(_deck.CardDeck);
+            Assert.AreEqual(52, _deck.CardDeck.Count);
+            Assert.AreEqual(52, _deck.CardDeck.Distinct().Count());
+            Assert.AreEqual(52, _deck.CardDeck.Select(c => c.ToString()).Distinct().Count());
+           
+        }
+
+        [Test]
+        public void ShuffleDeckSuccessTest()
+        {
+            
+            var position = 0;
+            var newPosition = 0;
+            var deck = new Deck();
+            var positions = _deck.CardDeck.ToDictionary(c => c, c => position++);
+
+
+            _shuffledDeck.CardDeck = _deck.ShuffleDeck(_deck.CardDeck);
+
+            foreach (var card in _shuffledDeck.CardDeck)
+            {
+                Console.WriteLine(card);
+            }
+
+            var positionChangeCount = _shuffledDeck.CardDeck.Select(c => positions[c] == newPosition++ ? 0 : 1).Sum();
+
+            // Assert
+            Assert.IsTrue(positionChangeCount > 52 * .9);
+            Assert.AreEqual(52, _shuffledDeck.CardDeck.Count);
+            Assert.AreEqual(52, _shuffledDeck.CardDeck.Distinct().Count());
+            Assert.AreEqual(52, _shuffledDeck.CardDeck.Select(c => c.ToString()).Distinct().Count());
+         
+        }
+
+    }
+}
---
+++ BlackjackTests/GameTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using BlackjackApp;
+using NUnit.Framework;
+
+namespace Tests
+{
+        [TestFixture]
+        public class GameTests
+        {
+            private Game _game;
+
+            [SetUp]
+            public void SetUp()
+            {
+                _game = new Game();
+            }
+
+            [Test]
+            public void CheckBlackJack_IsTrue_Test()
+            {
+                Player player = new Player();
+                player.Name = "Pooja";
+                PlayerGroup playerGroup = new PlayerGroup();
+                Deck deck = new Deck();
+                deck.CardDeck = deck.BuildCardDeck();
+                player.Hand.Add(deck.CardDeck[11]);
+                player.Hand.Add(deck.CardDeck[12]);
+                player.HandTotal = player.CountCards();
+                
+                playerGroup.PlayerList.Add(player);
+                Dealer house = new Dealer();
+                house.Hand.Add(deck.CardDeck[9]);
+                house.Hand.Add(deck.CardDeck[12]);
+                house.HandTotal = house.CountCards();
+
+                Assert.IsTrue(_game.CheckBlackJack(playerGroup, house));
+            }
+
+            [Test]
+            public void CheckBlackJack_IsFalse_Test()
+            {
+                Player player = new Player();
+                player.Name = "Pooja";
+                PlayerGroup playerGroup = new PlayerGroup();
+                Deck deck = new Deck();
+                deck.CardDeck = deck.BuildCardDeck();
+                player.Hand.Add(deck.CardDeck[11]);
+                player.Hand.Add(deck.CardDeck[12]);
+                player.HandTotal = player.CountCards();
+
+                playerGroup.PlayerList.Add(player);
+                Dealer house = new Dealer();
+                house.Hand.Add(deck.CardDeck[10]);
+                house.Hand.Add(deck.CardDeck[15]);
+                house.HandTotal = house.CountCards();
+
+                Assert.IsFalse(_game.CheckBlackJack(playerGroup, house));
+            }
+        }
+}
